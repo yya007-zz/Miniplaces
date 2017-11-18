7a41e1fd7dd63524e1208d8a6fc8de95192e30b1
Siqi Chen <Siqi Chen>
2017-11-18 03:07:49 -0500

    bug fix

--- model/tensorflow/alexnet_bn_train.py
+++ model/tensorflow/alexnet_bn_train.py
@@ -119,7 +119,7 @@ opt_data_val = {
 
 opt_data_test = {
     #'data_h5': 'miniplaces_256_val.h5',
-    'data_root': '../../data/test/',   # MODIFY PATH ACCORDINGLY
+    'data_root': '../../data/images/',   # MODIFY PATH ACCORDINGLY
     'data_list': '../../data/test.txt',   # MODIFY PATH ACCORDINGLY
     'load_size': load_size,
     'fine_size': fine_size,
