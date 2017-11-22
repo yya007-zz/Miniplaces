e9ebab1695fc4d35ad9d1dded7dae64390fc0d2e
Siqi Chen <Siqi Chen>
2017-11-21 21:02:40 -0500

    dd

--- model/tensorflow/resnet/resnet.py
+++ model/tensorflow/resnet/resnet.py
@@ -18,37 +18,37 @@ training_iters = 40000
 show_every = 50
 
 opt_data_train = {
-      #'data_h5': 'miniplaces_256_train.h5',
-      'data_root': '../../../data/images/',   # MODIFY PATH ACCORDINGLY
-      'data_list': '../../../data/train.txt', # MODIFY PATH ACCORDINGLY
-      'load_size': load_size,
-      'fine_size': fine_size,
-      'data_mean': data_mean,
-      'randomize': True,
-      'perm' : True
-      }
+  #'data_h5': 'miniplaces_256_train.h5',
+  'data_root': '../../../data/images/',   # MODIFY PATH ACCORDINGLY
+  'data_list': '../../../data/train.txt', # MODIFY PATH ACCORDINGLY
+  'load_size': load_size,
+  'fine_size': fine_size,
+  'data_mean': data_mean,
+  'randomize': True,
+  'perm' : True
+  }
 
   opt_data_val = {
-      #'data_h5': 'miniplaces_256_val.h5',
-      'data_root': '../../../data/images/',   # MODIFY PATH ACCORDINGLY
-      'data_list': '../../../data/val.txt',   # MODIFY PATH ACCORDINGLY
-      'load_size': load_size,
-      'fine_size': fine_size,
-      'data_mean': data_mean,
-      'randomize': False,
-      'perm' : False
-      }
+    #'data_h5': 'miniplaces_256_val.h5',
+    'data_root': '../../../data/images/',   # MODIFY PATH ACCORDINGLY
+    'data_list': '../../../data/val.txt',   # MODIFY PATH ACCORDINGLY
+    'load_size': load_size,
+    'fine_size': fine_size,
+    'data_mean': data_mean,
+    'randomize': False,
+    'perm' : False
+    }
 
   opt_data_test = {
-      #'data_h5': 'miniplaces_256_val.h5',
-      'data_root': '../../../data/images/',   # MODIFY PATH ACCORDINGLY
-      'data_list': '../../../data/test.txt',   # MODIFY PATH ACCORDINGLY
-      'load_size': load_size,
-      'fine_size': fine_size,
-      'data_mean': data_mean,
-      'randomize': False,
-      'perm' : False
-      }
+    #'data_h5': 'miniplaces_256_val.h5',
+    'data_root': '../../../data/images/',   # MODIFY PATH ACCORDINGLY
+    'data_list': '../../../data/test.txt',   # MODIFY PATH ACCORDINGLY
+    'load_size': load_size,
+    'fine_size': fine_size,
+    'data_mean': data_mean,
+    'randomize': False,
+    'perm' : False
+    }
 
 loader_train = DataLoaderDiskRandomize(**opt_data_train)
 loader_val = DataLoaderDisk(**opt_data_val)
