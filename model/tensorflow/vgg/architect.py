f8e97bd16cc88ba01e2c6f637932a852aa9775ff
Siqi Chen <Siqi Chen>
2017-11-21 20:05:44 -0500

    d

--- model/tensorflow/vgg/architect.py
+++ model/tensorflow/vgg/architect.py
@@ -5,7 +5,7 @@ import CNNModels
 
 class vgg_model:
     def __init__(self, x, y, keep_dropout, train_phase):
-        self.logits=VGG(x, keep_dropout, train_phase)
+        self.logits=tf.nn.softmax(VGG(x, keep_dropout, train_phase))
         self.loss=tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y, logits=self.logits))
 
 def VGG(x, keep_dropout, train_phase,num_classes=100):
